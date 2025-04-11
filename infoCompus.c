ac0fdae18115686531abaa1c881cf15981f21802
GonzaJM <gonzalojamardotuc@gmail.com>
2025-04-11 13:41:06 -0300

    creo un archivo que muestra las caracteristicas de 5 computadoras

---
+++ infoCompus.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+struct compu {
+ int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3)
+ int anio; // Año de fabricación (valor entre 2015 y 2024)
+ int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8)
+ char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres)
+};
+
+void listarPCs(struct compu pcs[], int cantidad);
+
+
+int main(){
+    int aleatorio, total = 5;
+    struct compu pcs[total];
+    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
+        "Pentium"};
+
+    srand(time(NULL));
+
+    for(int i = 0; i < 5; i++){
+        pcs[i].velocidad = (rand() % (3 - 1 + 1)) + 1;
+        pcs[i].anio = (rand() % (2024 - 2015 + 1)) + 2015;
+        pcs[i].cantidad_nucleos = (rand() % (8 - 1 + 1)) + 1;
+        aleatorio = (rand() % (5 - 0 + 1)) + 0;
+        pcs[i].tipo_cpu = tipos[aleatorio];
+    }
+
+    listarPCs(pcs, total);
+    
+    return 0;
+}
+
+void listarPCs(struct compu pcs[], int cantidad){
+    for(int i = 0; i < cantidad; i++){
+        printf("\nPC %d:", i + 1);
+        printf("\n%d", pcs[i].velocidad);
+        printf("\n%d", pcs[i].anio);
+        printf("\n%d", pcs[i].cantidad_nucleos);
+        printf("\n%s", pcs[i].tipo_cpu);
+        printf("\n");
+    }
+}
+
+
